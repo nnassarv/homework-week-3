ce24e7d7364ac7e8880d878f206a32c45ede9cfe
nanv <nabilnassarvelasquez@gmail.com>
2021-10-10 10:59:23 +0200

    update pw function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
 // Assignment Code
+// this is the 'generate password' button
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+// this is the function that generated new passwords
 function writePassword() {
   var password = generatePassword();
+  // this is where the new password will be displayed
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
